1b78729308e657edd0ed16882d8a68054a46681d
red <red@m.com>
2024-06-13 12:34:19 +0300

    init project

---
+++ src/main/java/com/gnod/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.gnod.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/gnod/chat/config/WebSocketEventListener.java
@@ -0,0 +1,33 @@
+package com.gnod.chat.config;
+
+import com.gnod.chat.chat.ChatMessage;
+import com.gnod.chat.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplates;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplates.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
